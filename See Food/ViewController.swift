ee8fe5d341ae89429362dba9282072dadbcb3fdc
Le Ngoc Lan Khue <lengoclankhue@Les-Mac.local>
2018-01-13 03:03:46 -0800

    Finished but we need our api key for blue mix visual recognition

--- See Food/ViewController.swift
+++ See Food/ViewController.swift
@@ -8,26 +8,36 @@
 
 import UIKit
 import VisualRecognitionV3
+import SVProgressHUD
+import Social
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    let apiKey = ""
+    let apiKey = "fbcd915e6875571676ce33843a6acc6f59db0e28"
     let version = "2018-01-13"
 
+    @IBOutlet weak var topBarImageView: UIImageView!
+    @IBOutlet weak var shareButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     let imagePicker = UIImagePickerController()
     var classificationResults : [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        shareButton.isHidden = true
         imagePicker.delegate = self
         
         
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        SVProgressHUD.show()
+        navigationItem.title = ""
+        topBarImageView.isHidden = true
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
             imageView.image = image
             imagePicker.dismiss(animated: true, completion: nil)
+            cameraButton.isEnabled = false
             let visualRecognition = VisualRecognition(apiKey: apiKey, version: version)
             let imageData = UIImageJPEGRepresentation(image, 0.01)
             let documentURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
@@ -39,17 +49,32 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 for index in 0..<classes.count {
                     self.classificationResults.append(classes[index].classification)
                 }
+                DispatchQueue.main.async {
+                    self.cameraButton.isEnabled = true
+                    SVProgressHUD.dismiss()
+                    self.shareButton.isHidden = false
+                }
                 if self.classificationResults.contains("hotdog"){
                     DispatchQueue.main.async {
                         self.navigationItem.title = "Hotdog !"
+                        self.navigationController?.navigationBar.barTintColor = UIColor.green
+                        self.navigationController?.navigationBar.isTranslucent = false
+                        self.topBarImageView.isHidden = false
+                        self.topBarImageView.image = UIImage(named: "hotdog")
+                        
                     }
                     
                 } else {
                     DispatchQueue.main.async {
                         self.navigationItem.title = "Not Hotdog !"
+                        self.navigationController?.navigationBar.barTintColor = UIColor.red
+                        self.navigationController?.navigationBar.isTranslucent = false
+                        self.topBarImageView.isHidden = false
+                        self.topBarImageView.image = UIImage(named: "not-hotdog")
                     }
                     
                 }
+                
             })
         } else {
             print("There was an error while taking image from camera")
@@ -65,7 +90,18 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
     }
     
-
+    @IBAction func shareTapped(_ sender: UIButton) {
+        if let vc = SLComposeViewController(forServiceType: SLServiceTypeTwitter) {
+            vc.setInitialText("My food is \(navigationItem.title)")
+            vc.add(#imageLiteral(resourceName: "hotdogBackground"))
+            present(vc, animated: true, completion: nil)
+        } else {
+            self.navigationItem.title = "Please log in to Twitter"
+        }
+        
+            
+    }
+    
 
 }
 
